4e3a87f42287493d640afcc4da23f1ceba11e971
jabdev <enriqueacacio31@gmail.com>
2024-11-07 22:15:25 -0500

    sale order controller and load database config

--- src/main/java/com/example/saleorderservice/LoadDatabase.java
+++ src/main/java/com/example/saleorderservice/LoadDatabase.java
@@ -1,17 +1,21 @@
 package com.example.saleorderservice;
-import com.example.saleorderservice.SaleOrderRepository;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.logging.Logger;
 
 class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
-
+    DateTimeFormat dateTime;
     @Bean
     CommandLineRunner initDatabase(SaleOrderRepository repository){
 
+        return args -> {
+            log.info("Preloading " + repository.save(new SaleOrder("Bob Marley", "w420", 10,"boxes", dateTime, true)));
+            log.info("Preloading " + repository.save(new SaleOrder("Jimi Hendrix", "lsd420", 50,"units", dateTime, true)));
+        };
 
 
     }
--- src/main/java/com/example/saleorderservice/SaleOrderController.java
+++ src/main/java/com/example/saleorderservice/SaleOrderController.java
@@ -1,9 +1,36 @@
 package com.example.saleorderservice;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.parser.Entity;
 
 @RestController
 public class SaleOrderController {
-    private final SaleOrderRepository repository:
+    private final SaleOrderRepository repository;
+
+    SaleOrderController(SaleOrderRepository repository){this.repository = repository;}
+
+    @GetMapping("/saleOrders")
+    CollectionModel<EntityModel<SaleOrder>> all(){
+        List<EntityModel<SaleOrder>> saleOrder = repository.findAll().stream()
+                .map(saleOrder1 -> EntityModel.of(saleOrder1,
+                        linkTo(methodOn(SaleOrderController.class).one(saleOrder1.getId())).withSelfRel())),
+                        linkTo(methodOn(SaleOrderController.class).all().withRel("saleOrder")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(saleOrder, linkTo(methodOn(SaleOrderController.class).all()).withSelfRel());
+
+    }
 }
